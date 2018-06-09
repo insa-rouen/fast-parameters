dd424cb6dc980b321277380dce733d5a828a248f
Hao BAI <hao.bai@insa-rouen.fr>
2018-06-10 01:43:22 +0200

    HB : Sort the result by lifetime fatigue damage.

--- NREL_5MW_Onshore/Output/DLC1.2/life.py
+++ NREL_5MW_Onshore/Output/DLC1.2/life.py
@@ -24,8 +24,9 @@
 #============================== Modules Personnels ==============================
 import fatigue
 #============================== Modules Communs ==============================
-import pickle, time, math
-from matplotlib import pyplot as plt
+import collections, pickle, time, math
+
+
 
 #-----------------------------------------------------------------------------------------
 #                                          PROGRAMME PRINCIPALE
@@ -41,12 +42,12 @@ class life(object):
         self.listOfSpeed = needForSpeed
         self.numberOfSpeed = len(needForSpeed)
         self.dlc = 'DLC1.2_NTM_'
-        self.distribution = distribution
-        self.propability = []
-        self.damage = {}
+        self.distribution = distribution # probability distribution
+        self.propability = [] # probability for each mean wind speed
+        self.damage = collections.OrderedDict()
         
         self.lifetime = 20 * 365*24*6 # designed lifetime of tower (per 10min)
-        self.life = {}
+        self.life = collections.OrderedDict()
 
         self.run()
 
@@ -56,22 +57,24 @@ class life(object):
         self.load()
         print("|- Calculating fatigue life with "+self.distribution+" distribution ...")
         self.assess()
+        print("|- Sorting data ...")
+        self.sort()
 
 
-    def assess(self):
-        if self.distribution == 'isoprobability':
-            self.isoprobability()
-        elif self.distribution == 'weibull':
-            self.weibull()
+    def assess(self, info=False):
+        if self.distribution == 'Uniform':
+            self.Uniform()
+        elif self.distribution == 'Weibull':
+            self.Weibull()
         else:
             print("|- [Error] The distribution law is not supported !")
             exit()
 
         for key, value in self.life.items():
+            self.life[key] = collections.OrderedDict()
             self.life[key] = {'D_i':[], 'Dlife_i':[], 'Dlife':0.0}
 
             for i in self.listOfSpeed:
-                
                 D_i = self.damage[i][key]['Dtotal'] 
                 index = self.listOfSpeed.index(i)
                 propa = self.propability[index]
@@ -81,25 +84,36 @@ class life(object):
                 self.life[key]['Dlife_i'].append(Dlife_i)
 
             self.life[key]['Dlife'] = sum(self.life[key]['Dlife_i'])
-            print(" - Total damage for " + key + "during lifetime is " + \
-                  str(self.life[key]['Dlife']))
+            if info: print(" - Total damage for " + key + "during lifetime is " + \
+                           str(self.life[key]['Dlife']))
 
-    def isoprobability(self):
+    def Uniform(self):
         self.propability = [1/float(self.numberOfSpeed)] * self.numberOfSpeed
  
 
-    def weibull(self, k=2, lamb=13.0):
-        # speedTry = [i/10. for i in range(251)]
-        # print speedTry
-
+    def Weibull(self, k=2, lamb=13.0):
         for speed in self.listOfSpeed:
             propability = (k/lamb) * (speed/lamb)**(k-1) * math.exp(-(speed/lamb)**k)
             self.propability.append(propability)
-
-        # plt.plot(speedTry, self.propability)
-        # plt.show()
-        # exit()
     
+    def sort(self, defaultShowCase=3):
+        sortedList = sorted(self.life.items(), key=lambda x:x[1]['Dlife'], reverse=True)
+        self.life = collections.OrderedDict()
+
+        print(" - The most "+str(defaultShowCase)+" dangerous spots:")
+        i = 1
+        for elem in sortedList:
+            self.life[elem[0]] = elem[1]
+
+            if elem[1]['Dlife'] >= 1:
+                print(" - "+elem[0]+" : "+str(elem[1]['Dlife']))
+            elif i <= defaultShowCase:
+                print(" - "+elem[0]+" : "+str(elem[1]['Dlife']))
+                i = i + 1
+
+
+
+
     def plotDlife(self, listOfSpot):
         from matplotlib import pyplot as plt
 
@@ -141,17 +155,17 @@ class life(object):
             print(" - "+filename)
 
         self.life = dict.fromkeys(self.damage[i].keys())
-        print("|- [OK] "+str(self.numberOfSpeed)+" files of wind speed have been loaded !")
+        print(" - "+str(self.numberOfSpeed)+" files of wind speed have been loaded !")
     
 
 def main():
     TIK = time.time()
     
-    # myLife = life(range(3,27,2), 'isoprobability')
-    myLife = life(range(3,27,2), 'weibull')
+    # myLife = life(range(3,27,2), 'Uniform')
+    myLife = life(range(3,27,2), 'Weibull')
     # print myLife.damage.keys()
     # myLife.plotDlife(['TwHt1@0   ', 'TwHt1@90  ', 'TwHt1@180 ', 'TwHt1@270 '])
-    myLife.plotDlife(['TwHt1@0   '])
+    # myLife.plotDlife(['TwHt1@0   '])
 
     TOK = time.time()
     print "Total Time(s) : ",TOK-TIK
