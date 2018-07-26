da88fd4b81d7aa01ba8312116d25911a59985247
Hao BAI <hao.bai@insa-rouen.fr>
2018-07-26 03:42:19 +0200

    HB : Fix a bug that can't repeat bash command in subprocess.call

--- Python/DLC2.3.py
+++ Python/DLC2.3.py
@@ -23,8 +23,9 @@
 #                                        MODULES
 #-----------------------------------------------------------------------------------------
 #============================== Modules Communs ==============================
-import os, re
+import os, re, numpy, time
 import fileinput # iterate over lines from multiple input files
+import shutil # high-level file operations
 import subprocess # call a bash command e.g. 'ls'
 from contextlib import contextmanager
 #============================== Modules Personnels ==============================
@@ -48,25 +49,37 @@ class DLC(object):
     """docstring for DLC"""
     def __init__(self, wind='', gridLossTime=[], outputFolder='/', toLog=False):
         self.wind =  wind
-        self.gridLossTime = {'start':gridLossTime[0], 'end':gridLossTime[1],
-                             'step':gridLossTime[2]}
+        self.time = {'start':gridLossTime[0],'end':gridLossTime[1],'step':gridLossTime[2]}
         self.outputFolder = outputFolder
         self.toLog = toLog
         # some fixed path
         self.__outputPath = os.path.expanduser(
                                      '~/Eolien/Parameters/NREL_5MW_Onshore/Output/DLC2.3')
-        self.__fstPath = '../Parameters/NREL_5MW_Onshore/DLC2.3_'
+        self.__fstPath = os.path.expanduser('~/Eolien/Parameters/NREL_5MW_Onshore')
+        self.__prefix = '/DLC2.3_'
         self.__servodyn = os.path.expanduser(
                             '~/Eolien/Parameters/NREL_5MW_Onshore/WT/ServoDyn_DLC2.3.dat')
 
-    def run(self):
-        with cd('~/Eolien/FAST/'):
-            command = 'fast {}{}.fst'.format(self.__fstPath, self.wind)
-            subprocess.call([os.getenv('SHELL'), '-i', '-c', command])
+    def run(self, loop=False):
+        if loop:
+            for t in numpy.arange(self.time['start'], self.time['end']+self.time['step'],
+                                  self.time['step']):
+                print("|- Simulating loss of grid at", t,"s ...")
+                self.change_gridloss(t)
+                self.__fast(False)
+                print(os.getcwd())
+                # self.move_and_rename(t)
+                print("|- [ OK ] Output file has been moved to {} folder !"
+                      .format(self.outputFolder))
+        else:
+            self.__fast()
 
-    def change_gridloss(self):
-        time = 110.0
-        # with open(self.__servodyn, 'r+') as f:
+
+    def move_and_rename(self, time):
+        shutil.move(self.__fstPath+self.__prefix+self.wind+'.out',
+                   self.__outputPath+self.outputFolder+'/'+self.wind+'/'+str(time)+'.out')
+
+    def change_gridloss(self, time):
         with fileinput.input(self.__servodyn, inplace=True, backup='.bak') as f:
             for line in f:                
                 if "TPitManS" in line:                    
@@ -79,9 +92,30 @@ class DLC(object):
     def __replace(self, string, keyword, value):
         position = string.find(keyword)
         substring = string[:position]
+        # replace float number (e.g. )
         newtime = re.sub('[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)', str(value), substring)
         newline = newtime + string[position:]
         return newline
+
+    def __fast(self, silence=False):            
+        with cd('~/Eolien/FAST'):
+            # command = 'fast {}{}{}.fst'.format(self.__fstPath, self.__prefix, self.wind)
+            command = './FAST_gdar64 {}{}{}.fst'.format(self.__fstPath, self.__prefix, self.wind)
+            if silence:
+                # print("|- Running FAST in silence mode ...")
+                subprocess.check_output([os.getenv('SHELL'), '-i', '-c', command])
+            else:
+                print("|- Running FAST ...")
+                # a = subprocess.check_output([os.getenv('SHELL'), '-i', '-c', command])
+                # print(a)
+                print(os.getcwd())
+                subprocess.call([command], shell=True)
+                subprocess.call([command], shell=True)
+                #When shell=False, args[:] is a command line to execute
+                #When shell=True, args[0] is a command line to execute and args[1:] is arguments to sh
+
+                # subprocess.call([os.getenv('SHELL'), '-i', '-c', 'exit'])
+                # subprocess.call([command])
 
 
                 
@@ -95,9 +129,9 @@ class DLC(object):
 #                                     MAIN FUNCTION
 #-----------------------------------------------------------------------------------------
 def main():
-    simu1 = DLC(wind='EOGR', gridLossTime=[110, 140.5, 10], outputFolder='/withoutTRD')
-    # simu1.run()
-    simu1.change_gridloss()
+    simu1 = DLC(wind='EOGR', gridLossTime=[110, 140.5, 20], outputFolder='/withoutTRD')
+    simu1.run(True)
+    # simu1.change_gridloss()
 
 
 #-----------------------------------------------------------------------------------------
