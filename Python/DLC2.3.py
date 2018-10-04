24641fcca6489d18178564e72eb426a9ecd83c7b
Hao BAI <hao.bai@insa-rouen.fr>
2018-10-04 11:24:18 +0200

    HB : [DLC2.3] Change timestep from 0.01s to 0.1s to avoid giant volume of data

--- Python/DLC2.3.py
+++ Python/DLC2.3.py
@@ -205,7 +205,7 @@ def run_multiprocess(wind, gridloss):
 def main():
     # ----- Running on multi processor
     TIK = time.time()    
-    timerange = frange(60, 90.5+0.01, 0.01)
+    timerange = frange(60, 90.5+0.01, 0.1)
 
     for wind in ['EOGR', 'EOGO', 'EOGR-2.0', 'EOGR+2.0']:
         print("========== {} ==========".format(wind))
