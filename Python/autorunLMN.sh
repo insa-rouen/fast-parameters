af61c80c1048290b131f6293bd11f9cbb5210305
Hao BAI <hbai@PC-LMN-1600B>
2018-11-02 20:41:28 +0100

    HB : Adapt to local computers in LMN office

--- Python/autorunLMN.sh
+++ Python/autorunLMN.sh
@@ -1,2 +1,2 @@
 cd $HOME/Eolien/Parameters/NREL_5MW_Onshore/Wind/DLC1.1
-find . -type f -name "NTM*.bts" -size 79M -exec rsync -avz --remove-source-files {} hbai@aster1.insa-rouen.fr:/home/hbai/Eolien/Parameters/NREL_5MW_Onshore/Wind/DLC1.1/ \;
+find . -type f -name "NTM*.bts" -size +70M -exec rsync -avz --remove-source-files {} hbai@aster1.insa-rouen.fr:/home/hbai/Eolien/Parameters/NREL_5MW_Onshore/Wind/DLC1.1/ \;
