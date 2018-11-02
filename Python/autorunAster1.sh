d914443bf8f5fe69f058009a8fcee790bb15b345
Hao BAI <hao.bai@insa-rouen.fr>
2018-11-02 19:26:38 +0100

    HB : [DLC1.1b] Add a bash file to aster1 for autorun

---
+++ Python/autorunAster1.sh
@@ -0,0 +1,4 @@
+#export PYTHONPATH=$PYTHONPATH:"${HOME}/Eolien/Parameters/Python/site-packages"
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"${HOME}/Eolien/FAST"
+cd $HOME/Eolien/Parameters/Python
+python3 ./aster1.py
