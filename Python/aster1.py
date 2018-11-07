517ed8cc8d7b45b7a9e67fdb929003d1d0c297d1
Hao BAI <hao.bai@insa-rouen.fr>
2018-11-07 02:55:45 +0100

    HB : [tools] Add codes to resume calculation

--- Python/aster1.py
+++ Python/aster1.py
@@ -28,7 +28,7 @@
 from tools import utils, server
 from DLC11b import runFAST_multiprocess, runStress_multiprocess, runFatigue_multiprocess
 #============================== Modules Communs ==============================
-
+import time
 
 
 #-----------------------------------------------------------------------------------------
@@ -48,10 +48,24 @@ from DLC11b import runFAST_multiprocess, runStress_multiprocess, runFatigue_mult
 #-----------------------------------------------------------------------------------------
 @utils.timer
 def main():
-    aster1 = server.Aster1('~/Eolien/Parameters/NREL_5MW_Onshore/Wind/DLC1.1/', False)
-    aster1.run(runFAST_multiprocess, True, False, False) # moveSource=True
-    aster1.run(runStress_multiprocess, 10, True) # thetaStep=90
-    aster1.run(runFatigue_multiprocess, False)
+    aster1 = server.Aster1('NTM',
+                           '~/Eolien/Parameters/NREL_5MW_Onshore/Wind/1000seeds.json',
+                           '~/Eolien/Parameters/NREL_5MW_Onshore/Wind/DLC1.1',
+                           '~/Eolien/Parameters/NREL_5MW_Onshore/Output/DLC1.1',
+                           False)
+    # aster1.initiate()
+    aster1.resume('TurbSim', outputFileSize=70*1024**2)
+
+    # aster1.resume('FAST', inputFileSize=70*1024**2, outputFileSize=90*1024**2)
+    # aster1.run(runFAST_multiprocess, True, True, True) # moveSource=True
+    # time.sleep(5)
+
+    # test.resume('Stress', inputFileSize=90*1024**2, outputFileSize=204*1024**2)
+    # aster1.run(runStress_multiprocess, 10, True) # thetaStep=90
+    # time.sleep(5)
+    
+    # test.resume('Fatigue', inputFileSize=204*1024**2, outputFileSize=20*1024)
+    # aster1.run(runFatigue_multiprocess, False)
 
 
 
