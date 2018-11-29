0b7732ce150026ea7ff9518036b4d34742f08d8d
Hao BAI <hao.bai@insa-rouen.fr>
2018-11-29 16:26:13 +0100

    HB : [TRD] [Optimization] Complete the definition of optimization problem

--- Python/Optimization/TRD_mode1.py
+++ Python/Optimization/TRD_mode1.py
@@ -27,11 +27,19 @@ from pyfast import DLC
 from tools import utils
 from pycrunch import amplitude as amp
 #*           ==================== Modules Communs ====================
+import sys
+import numpy as np
 from pyOpt import Optimization
 from pyOpt import ALPSO
-from mpi4py import MPI
-comm = MPI.COMM_WORLD
-myrank = comm.Get_rank()
+
+try:
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+    myrank = comm.Get_rank()
+except:
+    raise ImportError('mpi4py is required for parallelization')
+
+
 
 #!------------------------------------------------------------------------------
 #!                               CLASS DEFINITION
@@ -61,24 +69,27 @@ def get_peak_valley(filename):
     # ref_time = (75.20, 77.33, 78.94, 80.48, 82.04, 83.56, 85.1, 86.64, 88.17,
     #              89.7) # time that occurs extremum deflection
     ref_index = (1520, 1733, 1894, 2048, 2204, 2356, 2510, 2664, 2817, 2970) # index of time in list of time
-    ref_deflX = (0.5529, -0.8429, 0.495, -0.6029, 0.5595, -0.5398, 0.5607, -0.5233, 0.5454, -0.5063) # extremum deflection
+    # ref_deflX = (0.5529, -0.8429, 0.495, -0.6029, 0.5595, -0.5398, 0.5607, -0.5233, 0.5454, -0.5063) # extremum deflection
     
     # get peak and valley
     data = utils.readcsv(filename='./Output/'+filename, header=7, datarow=6009)
     deflX = data.get("YawBrTDxt")["Records"]
     trd_deflX = [deflX[i] for i in ref_index]
-    # calculate difference
-    diff = sum([abs(x)-abs(y) for x, y in zip(trd_deflX, ref_deflX)])
-    return diff
+    # norme des amplitutes: valeurs à minimiser
+    norme = np.linalg.norm(trd_deflX)
+    return norme
 
 def optiObj(optiVar):
     """ Optimization problem: objective function
     """
-
     # update objective function
     file = run_TRD(mode1=optiVar, gridloss=['EOG', 'O'], outputFolder='',
                    silence=1, echo=0)
     f = get_peak_valley(file)
+    g = []
+    fail = 0
+    print("[{}] Optimal variables: {}, Objectif function value: {}".format(myrank, optiVar, f))
+    sys.stdout.flush()
     return f, g, fail
 
 def optiInequalConstraint(parameter_list):
@@ -90,15 +101,44 @@ def optiEqualConstraint(parameter_list):
     """ Optimization problem: equality constraints
     """
     pass
+
+def optiProblem():
+    optiProb = Optimization('OptiTour',optiObj) # def problem
+    
+    # add groups of var
+    optiProb.addVarGroup('K',2,type='c',value=250., lower=0., upper=500.)
+    optiProb.addVarGroup('L',2,type='c',value=-250., lower=-500., upper=0.)
+    
+    # add name of objectif function
+    optiProb.addObj('f')
+
+    # configuration of ALPSO
+    optiDriv=ALPSO(pll_type='SPM')
+    optiDriv.setOption('dynInnerIter',1)
+    optiDriv.setOption('maxInnerIter',10)
+    optiDriv.setOption('maxOuterIter',200)
+    optiDriv.setOption('fileout',3) # summary+print
+    optiDriv.setOption('dtol',0.001)
+
+    # rerun
+    optiDriv(optiProb, store_hst=True)
+    # parallize calculation
+    if myrank==0 :
+        fsol = open('ALPSO_solution.txt', 'w')
+        fsol.write(str(optiProb.solution(0)))
+#        fsol.write(optProb.solution(1))
+        fsol.close()
+
+
 #!------------------------------------------------------------------------------
 #!                                MAIN FUNCTION
 #!------------------------------------------------------------------------------
 def main():
-    # file = run_TRD(mode1=[277.8, 26.7, -86.712, -17.0222], gridloss=['EOG', 'O'],
-    #                outputFolder='', silence=1, echo=1)
-    # get_peak_valley("EOG_O_277.826.7-86.712-17.0222.out")
+    file = run_TRD(mode1=[277.8, 26.7, -86.712, -17.0222], gridloss=['EOG', 'O'],
+                   outputFolder='', silence=1, echo=1)
+    get_peak_valley(file)
 
-    optiProb = Optimization('OptiTRD', optiObj)
+    # optiProblem()
 
 
 #!------------------------------------------------------------------------------
