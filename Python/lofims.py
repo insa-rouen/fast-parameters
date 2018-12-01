9ca850fcd81a94bd98bce0b8f3e1b2b404dce9cf
Hao BAI <hbai@PC-LOFIMS-T7610>
2018-12-01 22:13:18 +0100

    HB : [DLC1.1b] Correct the command to run TurbSim; Let the calculation begin !

--- Python/lofims.py
+++ Python/lofims.py
@@ -69,10 +69,10 @@ def main():
                            "~/Eolien/Parameters/NREL_5MW_Onshore/Output/DLC1.1",
                            echo=False)
     print(len(seeds))
-    lofims.seeds = seeds[:2] # set list of seeds manually
+    lofims.seeds = seeds[:1] # set list of seeds manually
 
     # TurbSim ------------------------------------------------------------------
-    lofims.run(runTurbSim_multiprocess, True, False) #silence, echo
+    lofims.run(runTurbSim_multiprocess) #silence, echo
     time.sleep(5)
 
     # FAST ---------------------------------------------------------------------
