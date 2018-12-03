5df7f8f48a01597193e3e839fd05503abef8607c
Hao BAI <hao.bai@insa-rouen.fr>
2018-12-03 23:51:06 +0100

    HB : Add a new script to run calculation on lmn-cs server

--- Python/lmn-cs.py
+++ Python/lmn-cs.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Run calculation on PC-LOFIMS-T7610
+# Run calculation on lmn-cs server
 # 
 #
 # Authors: Hao BAI (hao.bai@insa-rouen.fr)
-# Date: 01/12/2018
+# Date: 03/12/2018
 #
 # Comments:
-#     - 0.1: [01/12/18] Complete DLC1.1b for 10 000 simulations at 25 m/s
+#     - 0.1: [03/12/18] Run DLC1.1b for 10 000 simulations at 21 m/s
 #     
 # Description:
 #     
@@ -52,49 +52,44 @@ import psutil
 @utils.timer
 def main():
     # Check CPU usage ==========================================================
-    if psutil.cpu_percent() >= 80: return
+    if psutil.cpu_percent() >= 60: return
 
     # Load Seeds ===============================================================
-    # Recalculate TurbSim + FAST + Stress
-    with utils.cd("~/aster1/Wind"):
-        #with open("failedRunsFAST.json", "r") as f:
-        #    seeds1 = json.loads(f.read())
-        #with open("failedRunsStress.json", "r") as f:
-        #    seeds2 = json.loads(f.read())
-        with open("recomputedSeeds.json", "r") as f:
-            seeds3 = json.loads(f.read())
-    #seeds1.extend(seeds2)
-    seeds = seeds3
- 
+    with utils.cd('~/aster1/Wind'):
+        with open('10000seeds.json', 'r') as f:
+            seeds = json.loads(f.read())
+    liste = [s for s in seeds if s[0] == "NTM" and s[1] == "21"]
+    seeds = liste
+    
     # Run ======================================================================
-    lofims = server.Aster1(seeds,
-                           "~/Eolien/Parameters/NREL_5MW_Onshore/Wind/DLC1.1",
-                           "~/Eolien/Parameters/NREL_5MW_Onshore/Output/DLC1.1",
+    lmn_cs = server.Aster1(inputSeeds=seeds,
+                    windPath='~/Eolien/Parameters/NREL_5MW_Onshore/Wind/DLC1.1',
+                outputPath='~/Eolien/Parameters/NREL_5MW_Onshore/Output/DLC1.1',
                            echo=False)
-    lofims.seeds = seeds # set list of seeds manually
 
     # TurbSim ------------------------------------------------------------------
-    lofims.run(runTurbSim_multiprocess, True, False) #silence, echo
+    lmn_cs.run(runTurbSim_multiprocess, True, False) #silence, echo
     time.sleep(5)
 
     # FAST ---------------------------------------------------------------------
-    lofims.run(runFAST_multiprocess, True, False) #silence, echo
+    lmn_cs.run(runFAST_multiprocess, True, False) #silence, echo
     time.sleep(5)
     
     # Stress + Fatigue ---------------------------------------------------------
-    lofims.run(runStressFatigue_multiprocess, 10, False) # thetaStep, echo
+    lmn_cs.run(runStressFatigue_multiprocess, 10, False) # thetaStep, echo
+    # lmn_cs.resume('Fatigue', inputFileSize=85*1024**2, outputFileSize=20*1024, compress=True)
     time.sleep(5)
 
     # TurbSim + FAST + Stress + Fatigue ----------------------------------------
     # [ATTENTION] This will only overwrite recomputeALL.json
-    # lofims.resume("ALL", outputFileSize=20*1024)
+    # lmn_cs.resume('ALL', outputFileSize=20*1024)
 
-    lofims.finalcheck(btsFileSize=70*1024**2, outFileSize=90*1024**2, tgzFileSize=20*1024**2, damFileSize=20*1024)
+    lmn_cs.finalcheck(btsFileSize=70*1024**2, outFileSize=85*1024**2, tgzFileSize=20*1024**2, damFileSize=20*1024)
 
 
 
 #!------------------------------------------------------------------------------
 #!                                     RUNNING TEST
 #!------------------------------------------------------------------------------
-if __name__ == "__main__":
+if __name__ == '__main__':
         main()
