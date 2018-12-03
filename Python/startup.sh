d98e042dcd06c5499433dbc0e2737de6d73d2923
Hao BAI <hbai@PC-LMN-960>
2018-12-03 15:25:21 +0100

    HB : Add some comments in startup.sh so that it allows to mount disks of lmn-cs and PC-LOFIMS-T7610

--- Python/startup.sh
+++ Python/startup.sh
@@ -1,7 +1,17 @@
 # creat folder if it is not existant
 mkdir -p ~/aster1
+#mkdir -p ~/lofims
+#mkdir -p ~/lmn-cs
+
 # mount aster1 disk
 sleep 15
 sshfs -o allow_other hbai@aster1.insa-rouen.fr:/home/hbai/Eolien/Parameters/NREL_5MW_Onshore ~/aster1
+sleep 5
+#sshfs -o allow_other hbai@194.254.13.118:/home/hbai/Eolien/Parameters/NREL_5MW_Onshore ~/lofims
+sleep 5
+#sshfs -o allow_other hbai@lmn-cs.insa-rouen.fr:/home/hbai/Eolien/Parameters/NREL_5MW_Onshore ~/lmn-cs
+
 # to umount the remote disk
-# fusermount -u tmp
+# fusermount -u ~/aster1
+# fusermount -u ~/lofims
+# fusermount -u ~/lmn-cs
