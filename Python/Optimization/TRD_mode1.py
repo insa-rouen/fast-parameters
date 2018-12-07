c2df48535b7d252f268112667be2e7ef8f0061d1
Hao BAI <hao.bai@insa-rouen.fr>
2018-12-07 16:21:18 +0100

    HB : [Opti] Add variable "fail" in TRD_mode1.py in order to catch failed runs; [pyopti] Add a new package that re-abstract several DLC classes from pyfast in order to be used in optimization problems; [pyfast] A little improvement in naming bug file; [utils] Add a new function which allows to copy a file or a directory

--- Python/Optimization/TRD_mode1.py
+++ Python/Optimization/TRD_mode1.py
@@ -23,7 +23,7 @@
 #!                                   MODULES
 #!------------------------------------------------------------------------------
 #*         ==================== Modules Personnels ====================
-from pyfast import DLC
+from pyopti import DLC
 from tools import utils
 from pycrunch import amplitude as amp
 #*           ==================== Modules Communs ====================
@@ -52,14 +52,15 @@ except:
 #!------------------------------------------------------------------------------
 def run_TRD(mode1, gridloss, case='DLC2.3', outputFolder='', silence=False,
             echo=False):
-    temp = DLC.TRD(mode1=mode1,gridloss=gridloss,case=case,outputFolder=outputFolder, 
-               echo=echo)
+    temp = DLC.TRD(mode1=mode1,gridloss=gridloss,case=case,
+                   outputFolder=outputFolder, echo=echo)
     # set path
     temp.workPath = "~/Eolien/Parameters/Python/Optimization"
     temp.outputPath = "~/Eolien/Parameters/Python/Optimization/Output"
     temp.windPath = "~/Eolien/Parameters/Python/Optimization/Wind"
     temp.wtPath = "~/Eolien/Parameters/Python/Optimization/WT"
     temp.logPath = "~/Eolien/Parameters/Python/Optimization/log"
+    temp.tmpPath = "~/Eolien/Parameters/Python/Optimization/tmp"
     # run calculation
     temp.run(silence)
     return temp.outputFilename
@@ -72,7 +73,8 @@ def get_peak_valley(filename):
     # ref_deflX = (0.5529, -0.8429, 0.495, -0.6029, 0.5595, -0.5398, 0.5607, -0.5233, 0.5454, -0.5063) # extremum deflection
     
     # get peak and valley
-    data = utils.readcsv(filename='./Output/'+filename, header=7, datarow=6009)
+    data = utils.readcsv(filename='./Output/'+filename, header=7, datarow=6009,
+                         echo=False)
     deflX = data.get("YawBrTDxt")["Records"]
     trd_deflX = [deflX[i] for i in ref_index]
     # norme des amplitutes: valeurs à minimiser
@@ -83,12 +85,25 @@ def optiObj(optiVar):
     """ Optimization problem: objective function
     """
     # update objective function
-    file = run_TRD(mode1=optiVar, gridloss=['EOG', 'O'], outputFolder='',
-                   silence=1, echo=0)
-    f = get_peak_valley(file)
+    try:
+        file = run_TRD(mode1=optiVar, gridloss=['EOG', 'O'], outputFolder='',
+                       silence=1, echo=0)
+    except KeyboardInterrupt as e:
+        raise e
+    except:
+        fail = 1
+        print("[{}] ❌ Optimal variables: {}; FAST fails in calculation !".format(myrank, optiVar))
+        f = None
+    else:
+        f = get_peak_valley(file)
+        if np.isnan(f):
+            fail = 1
+            print("[{}] ⚠️ Optimal variables: {}; Objectif function returns: {} !".format(
+                myrank, optiVar, f))
+        else:
+            fail = 0
+            print("[{}] ✅ Optimal variables: {}; Objectif function value: {}".format(myrank, optiVar, f))
     g = []
-    fail = 0
-    print("[{}] Optimal variables: {}, Objectif function value: {}".format(myrank, optiVar, f))
     sys.stdout.flush()
     return f, g, fail
 
@@ -103,15 +118,14 @@ def optiEqualConstraint(parameter_list):
     pass
 
 def optiProblem():
+    """ Optimization problem: gather all features
+    """
     optiProb = Optimization('OptiTour',optiObj) # def problem
-    
     # add groups of var
     optiProb.addVarGroup('K',2,type='c',value=250., lower=0., upper=500.)
     optiProb.addVarGroup('L',2,type='c',value=-250., lower=-500., upper=0.)
-    
     # add name of objectif function
     optiProb.addObj('f')
-
     # configuration of ALPSO
     optiDriv=ALPSO(pll_type='SPM')
     optiDriv.setOption('dynInnerIter',1)
@@ -119,26 +133,30 @@ def optiProblem():
     optiDriv.setOption('maxOuterIter',200)
     optiDriv.setOption('fileout',3) # summary+print
     optiDriv.setOption('dtol',0.001)
-
     # rerun
     optiDriv(optiProb, store_hst=True)
     # parallize calculation
-    if myrank==0 :
+    if myrank == 0 :
         fsol = open('ALPSO_solution.txt', 'w')
         fsol.write(str(optiProb.solution(0)))
 #        fsol.write(optProb.solution(1))
         fsol.close()
 
 
+
 #!------------------------------------------------------------------------------
 #!                                MAIN FUNCTION
 #!------------------------------------------------------------------------------
 def main():
-    file = run_TRD(mode1=[277.8, 26.7, -86.712, -17.0222], gridloss=['EOG', 'O'],
-                   outputFolder='', silence=1, echo=1)
-    get_peak_valley(file)
+    runCode = 2
 
-    # optiProblem()
+    if runCode == 1: # for testing
+        file = run_TRD(mode1=[277.8, 26.7, -86.712, -17.0222],
+                       gridloss=['EOG', 'O'], outputFolder='', silence=1, echo=1)
+        get_peak_valley(file)
+
+    if runCode == 2: # for optimization
+        optiProblem()
 
 
 #!------------------------------------------------------------------------------
