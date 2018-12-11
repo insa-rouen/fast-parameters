282cc6aba77a2a3ab4f379ba7a863b7c29920153
Hao BAI <hao.bai@insa-rouen.fr>
2018-12-11 15:13:16 +0100

    HB : [DLC2.3b] Export maximal peak-to-peak amplitude in each wind speed and on each gridloss time; [pycrunch] Check input variable type in <method max_p2p_amplitude>; [tools] Add a new option in <function readcsv> to indicate the number of columns that will be read

--- NREL_5MW_Onshore/Output/DLC2.3/extrema.py
+++ NREL_5MW_Onshore/Output/DLC2.3/extrema.py
@@ -48,19 +48,20 @@ import multiprocessing
 #!------------------------------------------------------------------------------
 @utils.timer
 def main():
-    testCase = 2
+    testCase = 3
     if testCase == 1: # simple case
         with utils.cd("~/Eolien/Parameters/NREL_5MW_Onshore/Output/DLC2.3/withoutTRD/EOGO"):
             # bande of grid loss
             filelist = [str(t) for t in utils.frange(74.0, 76.1, 0.1)]
 
-            channels = ['YawBrTDxt','YawBrTDyt', 'YawBrFxp','YawBrFyp', 'TwHt4FLxt',
-                        'TwHt4FLyt', 'TwrBsFxt', 'TwrBsFyt', 'TwrBsMxt', 'TwrBsMyt']
+            channels = ['YawBrTDxt','YawBrTDyt', 'YawBrFxp','YawBrFyp',
+                        'TwHt4FLxt', 'TwHt4FLyt', 'TwrBsFxt', 'TwrBsFyt',
+                        'TwrBsMxt', 'TwrBsMyt']
 
             # # ----- Running on single processor
             # for file in filelist:
-            #     amplitude.find_peak_valley(file, header=7, datarow=6009, startline=12,
-            #                                channels=channels)
+            #     amplitude.find_peak_valley(file, header=7, datarow=6009,
+            #                                startline=12, channels=channels)
 
             # ----- Running on multi processor
             amp.find_peak_valley_multiprocess(filelist, 7, 6009, 12, channels)
@@ -87,7 +88,8 @@ def main():
                 resu = amp.Amplitude.max_p2p_amplitude(f,channels,'.ext',True)
                 all_results.extend(resu)
             # save to file/print to screen
-            output = amp.Amplitude.print(all_results, channels, 'max_amplitude.amp')
+            output = amp.Amplitude.print(all_results, channels,
+                                         'max_amplitude.amp')
             # compress files
             output_files = [elem["File"]+".out" for elem in output[channels[0]]]
             pool = multiprocessing.Pool()
@@ -96,8 +98,20 @@ def main():
             pool.close()
             pool.join()
 
-    # For testing ...
+    # get maxi amplitude for all time range and all speed
     if testCase == 3:
+        with utils.cd("~/lofims/Output/DLC2.3"):
+            all_files = ["{}_{}_{}".format(wind, v, t) for v in speedRange for t
+                         in timeRange]
+            all_results = []
+            for f in all_files:
+                resu = amp.Amplitude.max_p2p_amplitude(f, channels, '.ext',True)
+                all_results.extend(resu)
+            output = amp.Amplitude.print(all_results, channels,
+                                        'max_amplitude_ALL.amp')
+
+    # For testing ...
+    if testCase == 4:
         with utils.cd("~/Eolien/Parameters/Python/DLC2.3/Output/DLC2.3"):
             # find peak and valley
             filelist = ["{}_O_{}".format(wind, t) for t in timeRange]
