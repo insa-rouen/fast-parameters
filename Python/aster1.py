e26dfc5c7683df229b33716283ce7a2fd7ec8f0b
Hao BAI <hao.bai@insa-rouen.fr>
2019-05-11 21:53:24 +0200

    HB : [DLC1d] Change simulated time to 1 hour

--- Python/aster1.py
+++ Python/aster1.py
@@ -22,6 +22,7 @@
 #     - 1.2: [19/01/19] Run DLC1.3b for 10 000 simulations at 9 m/s
 #     - 1.3: [22/01/19] Run DLC1.3b for 10 000 simulations at 7 m/s
 #     - 1.4: [28/01/19] Run DLC1.3b for 10 000 simulations at 5 m/s
+#     - 2.0: [11/05/19] Change simulated time to 1 hour (60 min)
 #
 # Description:
 #     
@@ -37,10 +38,11 @@
 #!------------------------------------------------------------------------------
 #*============================= Modules Personnels =============================
 from tools import utils, server
-# from DLC11b import runFAST_multiprocess, runStress_multiprocess
-# from DLC11b import runFatigue_multiprocess, runStressFatigue_multiprocess
-from DLC13b import runTurbSim_multiprocess, runFAST_multiprocess
-from DLC13b import runStressFatigue_multiprocess, runALL_multiprocess
+from DLC11b import runFAST_multiprocess, runStress_multiprocess
+from DLC11b import runFatigue_multiprocess, runStressFatigue_multiprocess
+from DLC11b import runALL_multiprocess
+# from DLC13b import runTurbSim_multiprocess, runFAST_multiprocess
+# from DLC13b import runStressFatigue_multiprocess, runALL_multiprocess
 #*============================= Modules Communs ================================
 import time
 import json
@@ -69,49 +71,62 @@ def main():
     if psutil.cpu_percent() >= 60: return
 
     # Load Seeds ===============================================================
-    with utils.cd('~/Eolien/Parameters/NREL_5MW_Onshore/Wind'):
-        with open('10000seeds.json', 'r') as f:
-            seeds = json.loads(f.read())
-    liste = [s for s in seeds if s[0] == "ETM" and s[1] == "5"]
-    seeds = liste
+    # with utils.cd('~/Eolien/Parameters/NREL_5MW_Onshore/Wind'):
+    #     with open('10000seeds.json', 'r') as f:
+    #         seeds = json.loads(f.read())
+    # liste = [s for s in seeds if s[0] == "ETM" and s[1] == "5"]
+    # seeds = liste
 
-    
+    seeds = [["NTM",  "3",  "3333"],
+             ["NTM",  "5", "-5555"],
+             ["NTM",  "7",  "7777"],
+             ["NTM",  "9", "-9999"],
+             ["NTM", "11",  "1111"],
+             ["NTM", "13", "-1313"],
+             ["NTM", "15",  "1515"],
+             ["NTM", "17", "-1717"],
+             ["NTM", "19",  "1919"],
+             ["NTM", "21", "-2121"],
+             ["NTM", "23",  "2323"],
+             ["NTM", "25", "-2525"],
+            ]
     # Run ======================================================================
     aster1 = server.Aster1(inputSeeds=seeds,
-                    windPath='~/Eolien/Parameters/NREL_5MW_Onshore/Wind/DLC1.3',
-                outputPath='~/Eolien/Parameters/NREL_5MW_Onshore/Output/DLC1.3',
+                    windPath='~/Eolien/Parameters/NREL_5MW_Onshore/Wind/DLC1.1',
+                outputPath='~/Eolien/Parameters/NREL_5MW_Onshore/Output/DLC1.1',
                            echo=False)
     
-    # TurbSim + FAST + Stress + Fatigue ----------------------------------------
-    # [ATTENTION] This will only overwrite recomputeALL.json
-    # aster1.resume('ALL', outputFileSize=20*1024)
+    runMode = 1
+    if runMode == 1:
+        # All-In-One: TurbSim + FAST + Stress + Fatigue ------------------------
+        # [ATTENTION] This will only overwrite recomputeALL.json
+        # aster1.resume('ALL', outputFileSize=20*1024)
+        aster1.run(runALL_multiprocess, 10, "", True) # thetaStep, outputFolder,
+            # compress, silence, echo
 
-    # TurbSim ------------------------------------------------------------------
-    # [ATTENTION] This will only overwrite recomputeTurbSim.json
-    # aster1.resume('TurbSim', outputFileSize=70*1024**2)
+    if runMode == 2:
+        # TurbSim --------------------------------------------------------------
+        # [ATTENTION] This will only overwrite recomputeTurbSim.json
+        # aster1.resume('TurbSim', outputFileSize=70*1024**2)
 
-    # FAST ---------------------------------------------------------------------
-    aster1.resume('FAST', inputFileSize=70*1024**2)        
-    aster1.run(runFAST_multiprocess, True, False) #silence, echo
-    time.sleep(5)
-    
-    # Stress -------------------------------------------------------------------
-    # aster1.resume('Stress',inputFileSize=90*1024**2,outputFileSize=204*1024**2)
-    # aster1.run(runStress_multiprocess, 10, False) # thetaStep=90, echo
-    # time.sleep(5)
-    
-    # Stress + Fatigue ---------------------------------------------------------
-    aster1.resume('Fatigue', inputFileSize=85*1024**2, outputFileSize=20*1024,
-                  compress=True)
-    aster1.run(runStressFatigue_multiprocess, 10, False) # thetaStep, echo
-    aster1.resume('Fatigue', inputFileSize=85*1024**2, outputFileSize=20*1024,
-                  compress=True)
-    time.sleep(5)
+        # FAST -----------------------------------------------------------------
+        aster1.resume('FAST', inputFileSize=70*1024**2)        
+        aster1.run(runFAST_multiprocess, True, False) #silence, echo
+        time.sleep(5)
+        
+        # Stress + Fatigue -----------------------------------------------------
+        aster1.resume('Fatigue', inputFileSize=85*1024**2, 
+            outputFileSize=20*1024, compress=True)
+        aster1.run(runStressFatigue_multiprocess, 10, False) # thetaStep, echo
+        aster1.resume('Fatigue', inputFileSize=85*1024**2,     
+            outputFileSize=20*1024, compress=True)
+        time.sleep(5)
 
     # Final checking phase -----------------------------------------------------
-    # aster1.finalcheck(btsFileSize=70*1024**2, outFileSize=90*1024**2,
-    #                   tgzFileSize=20*1024**2, damFileSize=20*1024)
+    aster1.finalcheck(btsFileSize=70*1024**2, outFileSize=85*1024**2,
+                      tgzFileSize=20*1024**2, damFileSize=20*1024)
 
+    aster1.sendmail('hao.bai@insa-rouen.fr')
 
 
 #!------------------------------------------------------------------------------
